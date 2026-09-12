54ea753a6a20a1828439df640c28993e8ce787f3
jasonzli-DEV <jasonzxl@icloud.com>
2026-09-12 17:44:36 -0400

    fix unrelated failure

--- resources/scripts/components/App.tsx
+++ resources/scripts/components/App.tsx
@@ -82,12 +82,7 @@ function App() {
                 <LocaleLoader />
                 <ProgressBar />
                 <div className='mx-auto w-auto'>
-                    <BrowserRouter
-                        future={{
-                            v7_startTransition: true,
-                            v7_relativeSplatPath: true,
-                        }}
-                    >
+                    <BrowserRouter>
                         <SubuserPreviewProvider>
                             <SubuserPreviewFrame>
                                 <Routes>
